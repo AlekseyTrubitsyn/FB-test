351c886161e91e29731f56c664cf95564b1384a5
Aleksey Trubtisyn <at.8441451@yandex.ru>
2018-01-30 12:50:15 +0300

    fix clickable corner, optimise JS

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,8 @@
     const CLASS_SELECTED_PRE = 'catalog-item--selected-hover';
     const CLASS_SELECTED = 'catalog-item--selected';
     const CLASS_FOOTER = 'catalog-item__footer';
+    const CLASS_FOOTER_LINK = 'catalog-item-footer__link'
+    const CLASS_CLICKS_DISABLED = 'disable-click';
 
     let items = document.querySelectorAll('.catalog-item');
     let itemsArray = Array.prototype.slice.call(items);
@@ -18,30 +20,44 @@
     function selectionToggle(e) {
       e.preventDefault();
 
-      let isFooter = e.target.classList.contains(CLASS_FOOTER) || e.target.parentNode.classList.contains(CLASS_FOOTER);
-      let isLink = e.target.tagName === 'A';
+      if (!checkIsClickable(e)) return;
 
-      if (isFooter && !isLink) return;
+      let item = e.currentTarget;
 
-      let selected = this.classList.contains(CLASS_SELECTED);
-      let preSelected = this.classList.contains(CLASS_SELECTED_PRE);
+      let selected = item.classList.contains(CLASS_SELECTED);
+      let preSelected = item.classList.contains(CLASS_SELECTED_PRE);
 
-      if (preSelected) {
-        this.classList.remove(CLASS_SELECTED_PRE);
-        this.onmouseleave = null;
+      if (!(selected || preSelected)) {
+        item.classList.add(CLASS_SELECTED_PRE);
+        item.addEventListener('mouseleave', changePreToSelected);
 
-      } else if (selected) {
-        this.classList.remove(CLASS_SELECTED);
+      } else if (preSelected) {
+        item.classList.remove(CLASS_SELECTED_PRE);
+        item.removeEventListener('mouseleave', changePreToSelected);
 
       } else {
-        this.classList.add(CLASS_SELECTED_PRE);
-        this.onmouseleave = changePreToSelected;
-      }
+        item.classList.remove(CLASS_SELECTED);
 
-      function changePreToSelected() {
-        this.classList.remove(CLASS_SELECTED_PRE);
-        this.classList.add(CLASS_SELECTED);
-        this.onmouseleave = null;
+      }
+    }
+
+    function changePreToSelected(e) {
+      let item = e.currentTarget;
+
+      item.classList.remove(CLASS_SELECTED_PRE);
+      item.classList.add(CLASS_SELECTED);
+      item.removeEventListener('mouseleave', changePreToSelected);
+    }
+
+    function checkIsClickable(e) {
+      if (clicksDisabled(e.target)) return false;
+
+      if (clicksDisabled(e.target.parentNode) && !(e.target.tagName === 'A')) return false;
+
+      return true;
+
+      function clicksDisabled(element) {
+        return element.classList.contains(CLASS_CLICKS_DISABLED);
       }
     }
 })();
